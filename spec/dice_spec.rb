fb72097932a494af86ff70056f5fbc3c927fc032
Asad Khan <asad_khan47@hotmail.co.uk>
2018-11-01 13:45:44 +0000

    Refactored Code for Dice-App

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -2,23 +2,16 @@ require "dice"
 
 describe Dice do
 
-  it "Rolls a dice" do
-    expect(subject).to respond_to :roll
-  end
-
-  it "Rolls a number between 1 and 6" do
-    dice = Dice.new
-    expect(dice.roll(1)[0]).to be_between(1, 6)
-  end
-
   it "Expects arguments" do
-    dice = Dice.new
     expect(subject).to respond_to(:roll).with(1).argument
   end
 
-  it "Expects arguemnts to be multipled by dice rolled" do
-    dice = Dice.new
-    expect(dice.roll(1).length).to eq(1)
+  it "Rolls a number between 1 and 6" do
+    expect(subject.roll(1)[0]).to be_between(1, 6)
+  end
+
+  it "Checks the length of the array" do
+    expect(subject.roll(1).length).to eq(1)
   end
 
 end
